e77dd0110ed3e485436bf998594cd1b5bb7deb99
= <=>
2021-02-18 23:29:15 +0700

    setting header

--- lib/main.dart
+++ lib/main.dart
@@ -9,27 +9,22 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
+      title: 'MyApp',
+      theme: ThemeData(      
+        primarySwatch: Colors.blue,       
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("MyApp"),
+        ),
+        body: Container(alignment: Alignment.topCenter,
+               margin: EdgeInsets.all(20), 
+              child: Text("PERTANDINGAN HARI INI \t\t\t\t\t BERITA HARI INI")),),
     );
   }
 }
+
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
